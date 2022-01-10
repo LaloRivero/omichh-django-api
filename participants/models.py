3701ebc90ecd3c99079c93424e1975478d38750e
Eduardo Rivero <lalo@lalorivero.com>
2022-01-10 14:30:34 -0700

    School and Scores model

--- participants/models.py
+++ participants/models.py
@@ -15,15 +15,23 @@ class Profile(models.Model):
     category = models.CharField(max_length=4, choices=CATEGORY)
     tutor_name = models.CharField(max_length=100, blank=True, null=True)
     tutor_phone = models.PhoneNumberField(blank=True)
-    tutor_email = models.CharField(max_length=100, blank=True, null=True)
+    tutor_email = models.EmailField(max_length=100, blank=True, null=True)
 
 
 
 
 class School(models.Model):
-    pass
+    """ School model """
+
+    name = models.CharField(max_length=250)
+    direction = models.CharField(max_length=250)
+    principal_name = models.CharField(max_length=150, blank=True, null=True)
+    principal_email = models.EmailField(max_length=100, blank=True, null=True)
 
 
 
 class Scores(models.Model):
-    pass 
+    """ Scores Model Field """
+
+    test = models.IntegerField()
+    score = models.IntegerField()
