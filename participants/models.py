66b16814a687808b92db0c9a09518f92ae6a9bef
Eduardo Rivero <lalo@lalorivero.com>
2022-01-10 18:37:57 -0700

    fixing models and adding __str__ method to models

--- participants/models.py
+++ participants/models.py
@@ -2,21 +2,37 @@ from django.db import models
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
-class Profile(models.Model):
+class Participant(models.Model):
     """ Adding data to the Base user model """
     CATEGORY = [
         ('OMI','OMI'),
         ('OMIS','OMIS'),
         ('OMIP','OMIP'),
     ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    TYPE = [
+        ('student','alumno'),
+        ('teacher', 'maestro'),
+    ]
+    type_of_participant = models.CharField(max_length=50, choices=TYPE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150)
     temp_user_name_omegaup = models.CharField(max_length=100, blank=True, null=True)
     birthday = models.CharField(max_length=150)
-    phone = models.PhoneModelField(blank=True)
+    phone = PhoneNumberField(blank=True)
+    town = models.CharField(max_length=150)
     category = models.CharField(max_length=4, choices=CATEGORY)
     tutor_name = models.CharField(max_length=100, blank=True, null=True)
-    tutor_phone = models.PhoneNumberField(blank=True)
+    tutor_phone = PhoneNumberField(blank=True)
     tutor_email = models.EmailField(max_length=100, blank=True, null=True)
+
+    is_verified = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 
@@ -29,6 +45,8 @@ class School(models.Model):
     principal_name = models.CharField(max_length=150, blank=True, null=True)
     principal_email = models.EmailField(max_length=100, blank=True, null=True)
 
+    def __str__(self):
+        return self.name
 
 
 class Scores(models.Model):
@@ -36,3 +54,6 @@ class Scores(models.Model):
 
     test = models.IntegerField()
     score = models.IntegerField()
+
+    def __str__ (self):
+        return self.score
