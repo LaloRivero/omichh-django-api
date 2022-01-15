8b39f524bd0ea16d2071cb6bea0c372ed73c030e
Eduardo Rivero <lalo@lalorivero.com>
2022-01-15 09:28:06 -0700

    templates dir config

--- omichh_api/settings.py
+++ omichh_api/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = 'omichh_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
