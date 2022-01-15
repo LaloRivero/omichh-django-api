407cab4b1e46b0c7e7f43fe018c5b334a4fef79b
Eduardo Rivero <lalo@lalorivero.com>
2022-01-14 17:42:28 -0700

    Generic viewset config

--- participants/views.py
+++ participants/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
 
-# Create your views here.
+# Django REST framework
+from rest_framework import status, mixins, viewsets
+from rest_framework.pagination import PageNumberPagination
+
+# Models
+from participants.models import Participant
+
+# Serializer
+from participants.serializers.list_participants import ListParticipantSerializer
+
+class ParticipantViewSet(mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    """ List participants """
+
+    queryset = Participant.objects.filter(type_of_participant='student')
+    serializer_class = ListParticipantSerializer
