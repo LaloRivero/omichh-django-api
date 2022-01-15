1236742775fbbcb8dc132efc8f20df0f423ca14f
Eduardo Rivero <lalo@lalorivero.com>
2022-01-14 18:47:54 -0700

    Schools and Scores admin config

--- participants/admin.py
+++ participants/admin.py
@@ -4,7 +4,7 @@
 from django.contrib import admin
 
 # Models
-from participants.models import Participant
+from participants.models import Participant, School, Scores
 
 
 @admin.register(Participant)
@@ -22,3 +22,23 @@ class ParticipantAdmin(admin.ModelAdmin):
     search_fields = ['participant__category', 'email']
     list_filter = ['type_of_participant',
                    'category']
+
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    ''' Display schools data '''
+
+    list_display = ['id', 'name']
+    list_display_links = ['id']
+    list_editable = ['name']
+    search_fields = ['name']
+
+
+@admin.register(Scores)
+class ScoresAdmin(admin.ModelAdmin):
+    ''' Display scores data '''
+
+    list_display = ['id', 'test', 'score']
+    list_display_links = ['id']
+    list_editable = ['test', 'score']
+    search_fields = ['test']
