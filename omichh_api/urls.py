6869d4692db3fef3a1711228e0d97a7b8caa8c78
Eduardo Rivero <lalo@lalorivero.com>
2022-01-15 09:37:35 -0700

    adding schools endpoints

--- omichh_api/urls.py
+++ omichh_api/urls.py
@@ -6,9 +6,11 @@ from rest_framework.routers import DefaultRouter
 
 # Views
 from participants.views.participant import ParticipantViewSet
+from participants.views.school import SchoolViewSet
 
 router = DefaultRouter()
 router.register(r'participants', ParticipantViewSet, basename='participants' )
+router.register(r'schools', SchoolViewSet, basename='schools')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
