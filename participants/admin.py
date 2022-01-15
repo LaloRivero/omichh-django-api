b9aec3eb2e1eecc207cb9407a9c81278976c9863
Eduardo Rivero <lalo@lalorivero.com>
2022-01-14 18:07:41 -0700

    Admin config

--- participants/admin.py
+++ participants/admin.py
@@ -1,3 +1,24 @@
+''' Participants admin config '''
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from participants.models import Participant
+
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    ''' Display the participants data '''
+
+    list_display = ['id',
+                    'category',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'temp_user_name_omegaup']
+    list_display_links = ['id','email']
+    list_editable = ['first_name', 'last_name']
+    search_fields = ['participant__category', 'email']
+    list_filter = ['type_of_participant',
+                   'category']
