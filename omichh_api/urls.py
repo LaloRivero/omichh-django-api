6407afc4fd4a456066a37caed93824d12deba02f
Eduardo Rivero <lalo@lalorivero.com>
2022-01-16 20:26:00 -0700

    verify partcipant email endpoint

--- omichh_api/urls.py
+++ omichh_api/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 # Views
-from participants.views.participant import ParticipantViewSet
+from participants.views.participant import ParticipantViewSet, participant_verification
 from participants.views.school import SchoolViewSet
 
 router = DefaultRouter()
@@ -14,5 +14,6 @@ router.register(r'schools', SchoolViewSet, basename='schools')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('verify/', participant_verification, name="verify"),
     path('', include(router.urls))
 ]
