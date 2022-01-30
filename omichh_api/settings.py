23ffdcf1acbc7065067e553e522bbd17e76d0a4d
Eduardo Rivero <lalo@lalorivero.com>
2022-01-29 17:14:21 -0700

    Cors config

--- omichh_api/settings.py
+++ omichh_api/settings.py
@@ -61,6 +61,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'omichh_api.urls'
 
 TEMPLATES = [
